3c99aac91288f20cb5b19bcacdad8cc92ffb4b5b
Dean Tang <ttkk000@163.com>
2023-07-26 09:32:32 +0800

    feat:tips

--- leetcode/0001.Two-Sum/1. Two Sum.go
+++ leetcode/0001.Two-Sum/1. Two Sum.go
@@ -1,6 +1,7 @@
 package leetcode
 
 func twoSum(nums []int, target int) []int {
+	//存储上次遍历nums的v，k，如果遇到相同的v则覆盖，可以保证最近的索引输出
 	m := make(map[int]int)
 	for k, v := range nums {
 		if idx, ok := m[target-v]; ok {
